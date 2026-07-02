32ca4d3286f48a36eab4763f35cba4e8aed0d435
ibtihelbs <ibtihel.salah@outlook.fr>
2026-07-02 17:55:23 +0100

    open type

--- src/App.jsx
+++ src/App.jsx
@@ -14,8 +14,28 @@ export default function App() {
     <div className="app" dir="rtl">
       <Navbar bride={couple.bride} groom={couple.groom} />
       <Envelope couple={couple} event={event} verse={verse} />
-      <Schedule schedule={schedule} />
-      <VenueMap venue={event.venue} />
+      <div
+        className="paper"
+        style={{
+          position: "relative",
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+        }}
+      >
+        <img src="paper-2.jpg" alt="paper" className="paper-image" />
+        <div
+          className="paper-content"
+          style={{
+            position: "absolute",
+            zIndex: 3,
+            top: "10%",
+          }}
+        >
+          <Schedule schedule={schedule} />
+          <VenueMap venue={event.venue} />
+        </div>
+      </div>
       <RSVP rsvpDeadline={event.rsvpDeadline} />
       <footer className="footer">
         <p>
